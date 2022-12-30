fd91fac5ebe8dcd9e389b1e367d72cdb3523fbfe
micro <litwr@yandex.ru>
2022-12-30 08:17:32 +0300

    some cleaning, mandel8: the pcw is added as a draft

--- bbcmicro/ren1.php
+++
@@ -1,4 +0,0 @@
-<?php
-unlink("mandel-bbc-v2.zip") && printf("era 1 ok") || printf ("<p>all bad");
-#rename("mandel-bbc-v2.zzip", "mandel-bbc-v2.zip") && printf ("<p>ren 1 ok")|| printf ("<p>all bad");
-?>
